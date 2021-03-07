a924ad540faeb8254a5cbd8ddea6eacbedd342b3
Danny Maier <dannymaier@gmail.com>
2021-03-07 10:44:17 -0800

    finalized some css for the cards, they look much better, and added some more text content to flesh out the about me and project cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,20 +5,28 @@ function Card({key, id, image, title, summary, gitLink, actualLink}) {
     return (
 
  
-      <div className="col s12 m4">
+      <div className="card-container col s12 m4">
         <div className="card">
+{/* Title */}
+            <span className="card-title">{title}
+          <div className="divider" tabindex="-1"></div>
+            </span>
+          <br/>
+{/* image */}
           <div className="card-image">
             <img src={image}/>
           </div>
-          <div className="divider" tabindex="-1"></div>
-            <span className="card-title">{title}</span>
+          
+{/* Summary */}
           <div className="card-content">
             <p>{summary}</p>
           </div>
+{/* Links */}
           <div class="card-action">
             <a target="_blank" href={gitLink}>Repo</a>
             <a target="_blank" href={actualLink}>App</a>
         </div>
+
         </div>
       </div>
 
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,9 +6,14 @@ function About() {
             <h1 style={{marginLeft:"80px", color:"#37587a" }}>About Me:</h1>
             <div className="container">
                 <div className="row">
-                    <p 
-                        className="col "
-                    > After 10 years in the construction industry I'm looking to make a change and take on a new challenge. I'm learning how to code because I realized that my favorite part of my job was creating spreadsheet tools to aid in mechanical design that I shared with my colleagues. I find creating, testing and perfecting the tools, to be very satisfying. I hope to create more as a software developer in the future...</p>
+                    <p className="col ">
+                    After 10 years in the construction industry I'm looking to make a change and take on a new challenge. I'm learning how to code because I realized that my favorite part of my job was creating spreadsheet tools to aid in mechanical design that I shared with my colleagues. I find creating, testing and perfecting the tools, to be very satisfying. I hope to create more as a software developer in the future.</p>
+                    <p className="col ">
+                    I took the opportunity during Covid winter to diversity my skill set and joined the University of Washington Full Stack Web Developer Boot Camp while continuing to work full time. It was one of the most rewarding and challenging courses I've ever taken. I'm grateful to my husband and greyhound for helping me through it.
+                    </p>
+                    <p className="col ">
+                    Outside of work and school I like to paint, skate, game, travel and spend time with my friends and community.
+                    </p>
                 </div>
             </div>
         </div>
